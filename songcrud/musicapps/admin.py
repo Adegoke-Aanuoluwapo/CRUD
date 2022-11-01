a6068bc6480ffc0c37a61212bce97dfa65bc5525
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-11-01 13:19:13 +0100

    first commit

---
+++ songcrud/musicapps/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Song, Artist, Lyrics
+# Register your models here.
+admin.site.register(Song)
+admin.site.register(Artiste)
+admin.site.register(Lyrics)
